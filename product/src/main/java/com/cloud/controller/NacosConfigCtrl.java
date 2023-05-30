a75f1d7e3381b8899286c1d361f109f0352aff69
muscleHu <1209913500@qq.com>
2023-05-30 21:58:31 +0800

    测试nacos配置提交

---
+++ product/src/main/java/com/cloud/controller/NacosConfigCtrl.java
@@ -0,0 +1,29 @@
+package com.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nacos")
+@RefreshScope
+public class NacosConfigCtrl {
+
+    @Value("${test.name}")
+    private String testName;
+
+    @Value("${age}")
+    private String age;
+
+    @GetMapping("/getName")
+    public String getConfigInfo() {
+        return testName;
+    }
+
+    @GetMapping("/age")
+    public String getage() {
+        return age;
+    }
+}
