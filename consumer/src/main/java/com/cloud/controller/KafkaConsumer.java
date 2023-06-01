0e9d246c8fae1deb446ba1a13a53e75874258c38
muscleHu <1209913500@qq.com>
2023-06-01 17:32:20 +0800

    测试spring-kafka 提交

--- consumer/src/main/java/com/cloud/controller/KafkaConsumer.java
+++ consumer/src/main/java/com/cloud/controller/KafkaConsumer.java
@@ -1,16 +1,66 @@
 package com.cloud.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ConsumerAwareListenerErrorHandler;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class KafkaConsumer {
 
+    @Autowired
+    ConsumerFactory consumerFactory;
+
+    //异常处理器
+    @Bean
+    public ConsumerAwareListenerErrorHandler consumerAwareErrorHandler() {
+        return (message, exception, consumer) -> {
+            System.out.println("消费异常："+message.getPayload());
+            return null;
+        };
+    }
+
+    //消息过滤器
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory filterContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(consumerFactory);
+        // 被过滤的消息将被丢弃
+        factory.setAckDiscarded(true);
+        // 消息过滤策略
+        factory.setRecordFilterStrategy(consumerRecord -> {
+            //奇数被过滤
+            if (Integer.parseInt(consumerRecord.value().toString()) % 2 == 0) {
+                return false;
+            }
+            System.out.println(consumerRecord);
+            System.out.println("被过滤消息："+consumerRecord.value());
+            //返回true消息则被过滤
+            return true;
+        });
+        return factory;
+    }
+
     // 消费监听
-    @KafkaListener(topics = {"testTopic"})
+    @KafkaListener(topics = {"testTopic"},errorHandler = "consumerAwareErrorHandler")
     public void onMessage1(ConsumerRecord<?, ?> record){
+        //throw new Exception("简单消费-模拟异常");
         // 消费的哪个topic、partition的消息,打印出消息内容
         System.out.println("简单消费："+record.topic()+"-"+record.partition()+"-"+record.value());
+    }
+
+    //消息转发
+    @KafkaListener(topics = {"testTopic1"})
+    @SendTo("testTopic")
+    public String onMessage2(ConsumerRecord<?, ?> record) {
+        return record.value()+"-额外加了一段逻辑";
     }
 }
--- product/src/main/java/com/cloud/controller/KafkaProductCtrl.java
+++ product/src/main/java/com/cloud/controller/KafkaProductCtrl.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/kafkaProduct")
 public class KafkaProductCtrl {
@@ -16,7 +19,38 @@ public class KafkaProductCtrl {
 
     // 发送消息
     @GetMapping("/send/{message}")
-    public void sendMessage1(@PathVariable("message") String normalMessage) {
-        kafkaTemplate.send("testTopic", normalMessage);
+    public void sendMessage(@PathVariable("message") String message) {
+        kafkaTemplate.send("testTopic1", "1", message).addCallback(success -> {
+            // 消息发送到的topic
+            String topic = success.getRecordMetadata().topic();
+            // 消息发送到的分区
+            int partition = success.getRecordMetadata().partition();
+            // 消息在分区内的offset
+            long offset = success.getRecordMetadata().offset();
+            System.out.println("发送消息成功:" + topic + "-" + partition + "-" + offset);
+        }, failure -> {
+            System.out.println("发送消息失败:" + failure.getMessage());
+        });
+        /*kafkaTemplate.send("testTopic", message).addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("发送消息失败："+ex.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                System.out.println("发送消息成功：" + result.getRecordMetadata().topic() + "-"
+                        + result.getRecordMetadata().partition() + "-" + result.getRecordMetadata().offset());
+            }
+        });*/
     }
+
+    @GetMapping("/sendByTranscation/{message}")
+    public void sendMessageByTranscation(@PathVariable("message") String message) {
+        kafkaTemplate.executeInTransaction(operations -> {
+            operations.send("testTopic1","test executeInTransaction");
+            throw new RuntimeException("fail");
+        });
+    }
+
 }
